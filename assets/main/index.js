71c10529116c3205185da87f97b24594e923f1a2
hOMG <agiftedboy@163.com>
2025-11-12 15:30:45 +0800

    test

---
+++ assets/main/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/main",["./mainSceneCtrl.ts"],(function(){return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/mainSceneCtrl.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var n,e,r,c;return{setters:[function(t){n=t.inheritsLoose},function(t){e=t.cclegacy,r=t._decorator,c=t.Component}],execute:function(){var o;e._RF.push({},"d5391/1Nr1MtICBA0NSo9Ok","mainSceneCtrl",void 0);var i=r.ccclass;r.property,t("mainSceneCtrl",i("mainSceneCtrl")(o=function(t){function e(){return t.apply(this,arguments)||this}n(e,t);var r=e.prototype;return r.start=function(){},r.update=function(t){},e}(c))||o);e._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/main', 'chunks:///_virtual/main'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
